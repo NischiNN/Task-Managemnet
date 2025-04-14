b1ca0689baae0e38d3cde1abd9f2968211e7b8f1
nischitha <nischithan20@gmail.com>
2025-04-14 07:49:14 +0530

    Auto-generated commit

---
+++ chunk-UABGGPU4.js
@@ -0,0 +1,1 @@
+import{d as t}from"./chunk-MRJ2UCUH.js";var o=[{path:"login",title:"Login",loadComponent:()=>t(void 0,null,function*(){return(yield import("./chunk-PPEQ5QNT.js")).LoginComponent})},{path:"register",title:"Register",loadComponent:()=>t(void 0,null,function*(){return(yield import("./chunk-MGHGLZUM.js")).RegisterComponent})}];export{o as routes};
